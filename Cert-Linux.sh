bf0f171e0a64d00a76e9623ea6052268941cc1f6
Andrew <121936658+7gxycn08@users.noreply.github.com>
2024-08-09 01:34:09 +0400

    Add files via upload

---
+++ Cert-Linux.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: Must supply a domain and certificate name & ip address. Example: ./cert.sh domain.lan myCA 192.168.1.x"
+  exit 1
+fi
+
+DOMAIN=$1
+CANAME=$2
+IPADDR=$3
+
+echo "Generating CA key..."
+OPENSSL genrsa -des3 -out $CANAME.key 2048
+
+echo "Generating CA certificate..."
+OPENSSL req -x509 -new -nodes -key $CANAME.key -sha256 -days 1825 -out $CANAME.pem
+
+echo "Generating domain key..."
+OPENSSL genrsa -out $DOMAIN.key 2048
+
+echo "Generating domain CSR..."
+OPENSSL req -new -key $DOMAIN.key -out $DOMAIN.csr
+
+echo "Creating domain extension file..."
+cat > $DOMAIN.ext << EOF
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+[alt_names]
+IP.1 = ${IPADDR}
+EOF
+
+echo "Generating domain certificate..."
+OPENSSL x509 -req -in $DOMAIN.csr -CA $CANAME.pem -CAkey $CANAME.key -CAcreateserial -out $DOMAIN.crt -days 825 -sha256 -extfile $DOMAIN.ext
+
+echo "Certificate generation completed."
